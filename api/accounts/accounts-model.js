90d7b9520e82f643d2012ec758e46379d3991c7b
bryan <bryan.guner@gmail.com>
2021-07-12 19:13:11 -0400

    update

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,22 +1,34 @@
-const getAll = () => {
-  // DO YOUR MAGIC
-}
+const db = require('../../data/db-config');
 
-const getById = id => {
-  // DO YOUR MAGIC
-}
+const getAll = async () => {
+  return await db('accounts');
+};
 
-const create = account => {
-  // DO YOUR MAGIC
-}
+const getById = async id => {
+  return await db('accounts').where({id}).first();
+};
 
-const updateById = (id, account) => {
-  // DO YOUR MAGIC
-}
+const create = async account => {
+  const newId = await db('accounts').insert(account);
+  return await getById(newId);
+};
 
-const deleteById = id => {
-  // DO YOUR MAGIC
-}
+const updateById = async (id, account) => {
+  // UPDATE accounts SET name = newName, budget = newBudget WHERE id = argId;
+  await db('accounts')
+    .where({id})
+    .update(account);
+  return await getById(id); // cant use req.account here to make less db calls;
+};
+
+const deleteById = async id => {
+  // DELETE FROM accounts WHERE id = argID;
+  const deleted = await getById(id);
+  await db('accounts')
+    .where({id})
+    .del();
+  return deleted;
+};
 
 module.exports = {
   getAll,
@@ -24,4 +36,4 @@ module.exports = {
   create,
   updateById,
   deleteById,
-}
+};
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,54 @@
-const router = require('express').Router()
+const router = require('express').Router();
+const {
+  checkAccountPayload,
+  checkAccountNameUnique,
+  checkAccountId
+} = require('./accounts-middleware');
+const Accounts = require('./accounts-model');
 
+// getAll, getById, create, updateById, deleteById 
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  Accounts.getAll()
+    .then(accounts => {
+      res.json(accounts);
+    })
+    .catch(next);
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+router.get('/:id', checkAccountId, (req, res, next) => {
+  res.json(req.account);
+});
+
+router.post('/', checkAccountPayload, checkAccountNameUnique, (req, res, next) => {
+  Accounts.create(req.body)
+    .then(account => {
+      res.status(201).json(account);
+    })
+    .catch(next);
+});
+
+router.put('/:id', checkAccountId, checkAccountPayload, (req, res, next) => {
+  Accounts.updateById(req.params.id, req.body)
+    .then(updated => {
+      res.json(updated);
+    })
+    .catch(next);
+});
+
+router.delete('/:id', checkAccountId, (req, res, next) => {
+  Accounts.deleteById(req.params.id)
+    .then(deleted => {
+      res.json(deleted);
+      // res.json(req.account); //this uses less db calls
+    })
+    .catch(next);
+});
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
-})
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack
+  });
+});
 
 module.exports = router;
